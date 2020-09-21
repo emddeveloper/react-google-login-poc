bcb95a07c7aa82ec8f32e9cfbef2fb82d248f931
emddeveloper <emd.developer@gmail.com>
2020-09-21 16:34:48 +0530

    Working google login excluded .env file

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react"
+import "./App.css"
+import GoogleLogin from "react-google-login"
 function App() {
+  const responseGoogle = response => {
+    console.log(JSON.stringify(response))
+  }
+  if (!process.env.REACT_APP_GOOGLE_CLIENT_ID) {
+    alert("Please include .env file for your API keys")
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <>
+          <div>Welcome</div>
+          <GoogleLogin clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID} buttonText="Login" onSuccess={responseGoogle} onFailure={responseGoogle} cookiePolicy={"single_host_origin"} />
+        </>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
